61c9e9803dba912c8652f428ec0259ca065ec154
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2025-09-17 16:41:44 -0400

    Experiment 1

---
+++ write_junk_file.py
@@ -0,0 +1,23 @@
+#!/proj/sot/ska3/flight/bin/python
+
+from configparser import ConfigParser
+import argparse
+import os
+
+
+def get_options():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--mode', choices = ['primary', 'secondary', 'test'], required = True, help = "Determine configuration of script run.")
+    return parser.parse_args()
+
+def write_junk_file(dir):
+
+    with open(f"{dir}/test.txt", 'a+') as f:
+        f.write("I am writing junk.\n")
+
+if __name__ == "__main__":
+
+    args = get_options()
+    config = ConfigParser()
+    config.read('config.ini')
+    write_junk_file(config[args.mode]['OUTPUT_DIR'])
